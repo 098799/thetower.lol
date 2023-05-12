facecdbf658802d93c740b8fe259218424ff5d8e
Tomasz Grining <tomek.grining@gmail.com>
2023-05-12 15:41:39 +0200

    Forgotten files

---
+++ components/namechangers.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import streamlit as st
+
+from dtower.tourney_results.data import get_sus_ids
+
+
+def compute_namechangers(df, options=None):
+    df = df[~df.id.isin(get_sus_ids())]
+
+    combined_data = []
+
+    for id, data in df.groupby("id"):
+        if len(data.tourney_name.unique()) == 1:
+            continue
+
+        real_name = data.real_name.iloc[0]
+        how_many_rows = len(data)
+        how_many_names = len(data.tourney_name.unique())
+        last_performance = data[-5:].wave.mean()
+
+        combined_data.append(
+            {
+                "real_name": real_name,
+                "no_in_champ": how_many_rows,
+                "namechanged_times": how_many_names,
+                "mean_last_5_tourneys": int(round(last_performance, 0)),
+            }
+        )
+
+    new_df = pd.DataFrame(combined_data)
+    new_df = new_df.sort_values("namechanged_times", ascending=False).reset_index(drop=True)
+
+    to_be_displayed = new_df.style.apply(lambda row: [f"color: {df[df.real_name == row.real_name].iloc[-1].name_role_color}", None, None, None], axis=1)
+
+    st.dataframe(to_be_displayed, use_container_width=True, height=800)
