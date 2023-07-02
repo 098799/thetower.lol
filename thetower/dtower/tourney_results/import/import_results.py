d6edd3ef3cc800d7130ea85b0efe3319746ef810
Tomasz Grining <tomek.grining@gmail.com>
2023-07-02 18:15:42 +0200

    Current version of get/import scripts with refactor and comments

--- thetower/dtower/tourney_results/import/import_results.py
+++ thetower/dtower/tourney_results/import/import_results.py
@@ -14,39 +14,27 @@ from glob import glob
 
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-from dtower.tourney_results.constants import champ, leagues, us_to_jim
+from dtower.tourney_results.constants import leagues, us_to_jim
 from dtower.tourney_results.get_results import get_file_name, get_last_date
 from dtower.tourney_results.models import TourneyResult
 
 while True:
     for league in leagues:
-        print(f"{datetime.datetime.now()} Trying to upload results for {league=} and {get_last_date()=}")
+        last_date = get_last_date()
 
-        current_datetime = get_last_date()
+        print(f"{datetime.datetime.now()} Trying to upload results for {league=} and {last_date=}")
 
-        if "T" in current_datetime:
-            print("Tourney day, continuing...")
+        last_results = TourneyResult.objects.filter(date=last_date, league=league)
 
-            continue  # change me
-
-            if league != champ:
-                continue
-
-            current_date = current_datetime.split("T")[0]
-        else:
-            last_results = TourneyResult.objects.filter(date=get_last_date(), league=league)
-
-            if last_results:
-                print("Nothing new")
-                continue
-
-            current_date = current_datetime
+        if last_results:
+            print(f"Nothing new, results are already uploaded for {last_date=}")
+            continue
 
         print("Something new")
-        last_files = sorted(glob(f"/root/tourney/results_cache/{us_to_jim[league]}/{current_date}*"))
+        last_files = sorted(glob(f"/root/tourney/results_cache/{us_to_jim[league]}/{last_date}*"))
 
         if not last_files:
-            print("maybe later")
+            print("Apparently we're checking the files before the download script could get them, try later.")
             continue
 
         last_file = last_files[-1]
@@ -66,14 +54,15 @@ while True:
         )
         print("Creating tourney_result")
         TourneyResult.objects.update_or_create(
-            date=current_date,
+            date=last_date,
             league=league,
             defaults=dict(
                 result_file=csv_file,
             ),
         )
 
-        if "T" not in current_datetime:
-            subprocess.call("systemctl restart streamlit", shell=True)
+        # This is how we invalidate the cache on the hidden version of the site http://thetower.lol:8502/
+        # Crude, there should be a better way
+        subprocess.call("systemctl restart streamlit", shell=True)
 
     time.sleep(3600)
