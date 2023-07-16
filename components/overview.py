848c8b7201e1c32f2e4f4144347f6ab609864b74
Tomasz Grining <tomek.grining@gmail.com>
2023-07-16 23:00:33 +0200

    Experimental relic/avatar support

--- components/overview.py
+++ components/overview.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
 
-from components.results import Results, compute_results
-from dtower.tourney_results.constants import Graph, Options, league_to_folder, silver
+from components.results import Results
+from dtower.tourney_results.constants import Graph, Options, league_to_folder, leagues
 from dtower.tourney_results.data import load_tourney_results
 
 
@@ -10,7 +10,9 @@ def compute_overview(dfs, options: Options):
     overall_df = pd.concat(dfs)
     last_tourney = sorted(overall_df.date.unique())[-1]
 
-    for df in dfs:
+    for df, league in zip(dfs, leagues):
+        st.header(league)
+
         filtered_df = df[df.date == last_tourney]
         filtered_df = filtered_df.reset_index(drop=True)
 
@@ -20,7 +22,8 @@ def compute_overview(dfs, options: Options):
         results = Results(filtered_df, options)
         to_be_displayed = results.prepare_data(filtered_df, how_many=10)
         to_be_displayed_styler = results.regular_preparation(to_be_displayed, filtered_df)
-        st.dataframe(to_be_displayed_styler, use_container_width=True)
+        st.write(to_be_displayed_styler.hide(axis="index").to_html(escape=False), unsafe_allow_html=True)
+        # st.dataframe(to_be_displayed_styler, use_container_width=True)
 
 
 if __name__ == "__main__":
