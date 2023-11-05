9546a99b988dfd2ad6c99fee804ca5d992d8df1e
Tomasz Grining <tomek.grining@gmail.com>
2023-11-05 12:31:42 +0100

    Added search term in table in comparison + trying to make comparison a bit cleaner

--- components/comparison.py
+++ components/comparison.py
@@ -25,6 +25,8 @@ from dtower.tourney_results.data import get_id_lookup, get_patches, get_player_l
 from dtower.tourney_results.formatting import BASE_URL, color_top_18, make_player_url
 from dtower.tourney_results.models import PatchNew as Patch
 
+sus_ids = set(SusPerson.objects.filter(sus=True).values_list("player_id", flat=True))
+
 
 def compute_comparison(df, options: Options):
     hidden_features = os.environ.get("HIDDEN_FEATURES")
@@ -39,8 +41,6 @@ def compute_comparison(df, options: Options):
     first_choices, all_real_names, all_tourney_names, all_user_ids, _ = get_player_list(df)
     player_list = [""] + first_choices + sorted(all_real_names | all_tourney_names) + all_user_ids
 
-    sus_ids = set(SusPerson.objects.filter(sus=True).values_list("player_id", flat=True))
-
     if not hidden_features:
         sus_nicknames = set(SusPerson.objects.filter(sus=True).values_list("name", flat=True))
         player_list = [player for player in player_list if player not in sus_ids | sus_nicknames]
@@ -53,11 +53,7 @@ def compute_comparison(df, options: Options):
 
     st.code(f"http://{BASE_URL}/Player%20Comparison?" + urlencode({"compare": users}, doseq=True))
 
-    datas = []
-
-    patches_options = sorted(
-        [patch for patch in get_patches() if patch.version_minor], key=lambda patch: patch.start_date, reverse=True
-    )
+    patches_options = sorted([patch for patch in get_patches() if patch.version_minor], key=lambda patch: patch.start_date, reverse=True)
     graph_options = [options.default_graph.value] + [
         value for value in list(Graph.__members__.keys()) + patches_options if value != options.default_graph.value
     ]
@@ -65,38 +61,37 @@ def compute_comparison(df, options: Options):
 
     id_mapping = get_id_lookup()
 
-    colors, stratas, tbdf = add_user_data_to_datas(
-        all_real_names, all_tourney_names, all_user_ids, datas, df, first_choices, id_mapping, patch, sus_ids, users
-    )
+    datas = create_plot_datas(all_real_names, all_tourney_names, all_user_ids, df, first_choices, id_mapping, patch, users)
 
     if not datas:
         return
 
-    datas = sorted(datas, key=lambda datum: max(datum.wave), reverse=True)
+    datas = sorted([(data, user) for data, user in datas], key=lambda datum: max(datum[0].wave), reverse=True)
 
     summary = pd.DataFrame(
         [
             [
                 data.real_name.unique()[0],
+                user,
                 len(data),
                 max(data.wave),
                 int(round(median(data.wave), 0)),
                 int(round(stdev(data.wave), 0)),
                 min(data.wave),
             ]
-            for data in datas
+            for data, user in datas
         ],
-        columns=["User", "No. times in champ", "total PB", "Median", "Stdev", "Lowest score on record"],
+        columns=["Name", "Search term", "No. tourneys", "total PB", "Median", "Stdev", "Lowest score"],
     )
-    summary.set_index(keys="User")
+    summary.set_index(keys="Name")
 
     if options.links_toggle:
-        to_be_displayed = summary.style.format(make_player_url, subset=["User"]).to_html(escape=False)
+        to_be_displayed = summary.style.format(make_player_url, subset=["Name"]).to_html(escape=False)
         st.write(to_be_displayed, unsafe_allow_html=True)
     else:
         st.dataframe(summary, use_container_width=True)
 
-    pd_datas = pd.concat(datas)
+    pd_datas = pd.concat([data for data, _ in datas])
     pd_datas["bcs"] = pd_datas.bcs.map(lambda bc_qs: " / ".join([bc.shortcut for bc in bc_qs]))
 
     last_5_tourneys = sorted(pd_datas.date.unique())[-5:][::-1]
@@ -106,21 +101,16 @@ def compute_comparison(df, options: Options):
             [
                 data.real_name.unique()[0],
             ]
-            + [
-                wave_serie.iloc[0] if not (wave_serie := data[data.date == date].wave).empty else 0
-                for date in last_5_tourneys
-            ]
-            for data in datas
+            + [wave_serie.iloc[0] if not (wave_serie := data[data.date == date].wave).empty else 0 for date in last_5_tourneys]
+            for data, _ in datas
         ],
-        columns=["User", *[f"{date.month}/{date.day}: {bc}" for date, bc in zip(last_5_tourneys, last_5_bcs)]],
+        columns=["Name", *[f"{date.month}/{date.day}: {bc}" for date, bc in zip(last_5_tourneys, last_5_bcs)]],
     )
 
-    last_results = last_results.style.apply(
-        lambda row: [None, *[color_top_18(wave=row[i + 1]) for i in range(len(last_5_tourneys))]], axis=1
-    )
+    last_results = last_results.style.apply(lambda row: [None, *[color_top_18(wave=row[i + 1]) for i in range(len(last_5_tourneys))]], axis=1)
 
     if options.links_toggle:
-        to_be_displayed = last_results.format(make_player_url, subset=["User"]).to_html(escape=False)
+        to_be_displayed = last_results.format(make_player_url, subset=["Name"]).to_html(escape=False)
         st.write(to_be_displayed, unsafe_allow_html=True)
     else:
         st.dataframe(last_results, use_container_width=True)
@@ -132,7 +122,7 @@ def compute_comparison(df, options: Options):
     min_ = min(pd_datas.wave)
     max_ = max(pd_datas.wave)
 
-    not_sure_what_to_call_this(colors, fig, max_, min_, pd_datas, stratas, tbdf)
+    enrich_plot(fig, max_, min_, pd_datas)
 
     st.plotly_chart(fig)
 
@@ -146,13 +136,11 @@ def compute_comparison(df, options: Options):
         st.json(data)
 
 
-def add_user_data_to_datas(
-    all_real_names, all_tourney_names, all_user_ids, datas, df, first_choices, id_mapping, patch, sus_ids, users
-):
+def create_plot_datas(all_real_names, all_tourney_names, all_user_ids, df, first_choices, id_mapping, patch, users):
+    datas = []
+
     for user in users:
-        player_df = loop_over_search_choices_for_user(
-            all_real_names, all_tourney_names, all_user_ids, df, first_choices, id_mapping, user
-        )
+        player_df = get_player_df(all_real_names, all_tourney_names, all_user_ids, df, first_choices, id_mapping, user)
 
         if len(player_df.id.unique()) >= 2:
             aggreg = player_df.groupby("id").count()
@@ -169,39 +157,30 @@ def add_user_data_to_datas(
         if id_ in sus_ids:
             st.error(f"Player {real_name} is considered sus.")
 
-        colors, patch_df, stratas = handle_patch_colors(df, patch, player_df)
+        patch_df = get_patch_df(df, player_df, patch)
 
         tbdf = patch_df.reset_index(drop=True)
 
         if len(tbdf) >= 2:
-            datas.append(tbdf)
-    return colors, stratas, tbdf
+            datas.append((tbdf, user))
+
+    return datas
 
 
-def not_sure_what_to_call_this(colors, fig, max_, min_, pd_datas, stratas, tbdf):
-    for color_, strata in zip(colors, stratas):
-        if max_ > strata > min_:
-            fig.add_hline(
-                y=strata,
-                line_color=color_,
-                line_dash="dash",
-                opacity=0.4,
-            )
+def enrich_plot(fig, max_, min_, pd_datas):
     for index, (start, version_minor, version_patch, interim) in enumerate(
         Patch.objects.all().values_list("start_date", "version_minor", "version_patch", "interim")
     ):
         name = f"0.{version_minor}.{version_patch}"
         interim = "interim" if interim else ""
 
-        if start < pd_datas.date.min() - datetime.timedelta(days=2) or start > pd_datas.date.max() + datetime.timedelta(
-            days=3
-        ):
+        if start < pd_datas.date.min() - datetime.timedelta(days=2) or start > pd_datas.date.max() + datetime.timedelta(days=3):
             continue
 
         fig.add_vline(x=start, line_width=3, line_dash="dash", line_color="#888", opacity=0.4)
         fig.add_annotation(
             x=start,
-            y=tbdf.wave.max() - 300 * (index % 2 + 1),
+            y=pd_datas.wave.max() - 300 * (index % 2 + 1),
             text=f"Patch {name}{interim} start",
             showarrow=True,
             arrowhead=1,
@@ -225,9 +204,17 @@ def handle_patch_colors(df, patch, player_df):
     return colors, patch_df, stratas
 
 
-def loop_over_search_choices_for_user(
-    all_real_names, all_tourney_names, all_user_ids, df, first_choices, id_mapping, user
-):
+def get_patch_df(df, player_df, patch):
+    if isinstance(patch, Patch):
+        patch_df = player_df[player_df.patch == patch]
+    elif patch == Graph.last_16.value:
+        patch_df = player_df[player_df.date.isin(df.date.unique()[-16:])]
+    else:
+        patch_df = player_df
+    return patch_df
+
+
+def get_player_df(all_real_names, all_tourney_names, all_user_ids, df, first_choices, id_mapping, user):
     if user in (set(first_choices) | all_real_names | all_tourney_names):
         player_df = df[(df.real_name == user) | (df.tourney_name == user)]
     elif user in all_user_ids:
