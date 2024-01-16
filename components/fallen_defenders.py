623898d145b7303ebf09bf7b9d26f38a2f591d62
Tomasz Grining <tomek.grining@gmail.com>
2024-01-16 07:37:59 +0100

    Actually commit fallen defenders

---
+++ components/fallen_defenders.py
@@ -0,0 +1,79 @@
+import datetime
+
+import pandas as pd
+import streamlit as st
+
+from dtower.tourney_results.constants import champ, league_to_folder
+from dtower.tourney_results.data import get_sus_ids, load_tourney_results
+
+
+def compute_fallen_defenders(df):
+    def style_df(df):
+        return df[["Player", "When (days)", "Best wave", "Best #"]].style.apply(styling, axis=1)
+
+    st.header("Fallen defenders")
+    st.write("We're honoring the fallen defenders of yesteryear.")
+
+    today = datetime.date.today()
+    fallen = []
+    reviewed = set()
+
+    for date, ddf in df.groupby("date"):
+        sdf = ddf[ddf.position <= 50]
+
+        for player_id in sdf.id:
+            if player_id in reviewed:
+                continue
+
+            pdf = df[df.id == player_id]
+            last_date = pdf.date.max()
+            last_seen = last_date - today
+
+            if last_seen < datetime.timedelta(days=-14):
+                best_wave = pdf.wave.max()
+                wave_color = pdf[pdf.wave == best_wave].wave_role_color.iloc[0]
+                best_position = pdf.position.min()
+                position_color = pdf[pdf.position == best_position].position_role_color.iloc[0]
+                player = pdf.iloc[0].real_name
+
+                fallen.append(
+                    {
+                        "Player": player,
+                        "days": last_seen.days,
+                        "When (days)": -last_seen.days,
+                        "Best wave": best_wave,
+                        "wave_color": wave_color,
+                        "Best #": best_position,
+                        "position_color": position_color,
+                    }
+                )
+
+            reviewed.add(player_id)
+
+    fallen_df = pd.DataFrame(fallen).sort_values(["Best #", "days", "Best wave"]).reset_index(drop=True)
+
+    fallen_champ = fallen_df[fallen_df["Best #"] == 1]
+    fallen_other = fallen_df[fallen_df["Best #"] != 1]
+
+    styling = lambda row: [
+        None,
+        None,
+        f"color: {fallen_df[fallen_df['Player']==row['Player']].wave_color.iloc[0]}",
+        f"color: {fallen_df[fallen_df['Player']==row['Player']].position_color.iloc[0]}",
+    ]
+
+    tbdf_champ = style_df(fallen_champ)
+    tbdf_other = style_df(fallen_other)
+
+    champ_col, other_col = st.columns([1, 1])
+    champ_col.write("All hail fallen champions 🫡")
+    other_col.write("May your labs still chug along")
+
+    champ_col.dataframe(tbdf_champ.hide("Best wave", axis=1), hide_index=True)
+    other_col.dataframe(tbdf_other, hide_index=True)
+
+
+if __name__ == "__main__":
+    df = load_tourney_results(league_to_folder[champ])
+    df = df[~df.id.isin(get_sus_ids())]
+    compute_fallen_defenders(df)
