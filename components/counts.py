9f7d55c24b89514e872dd8103d8f42ce910a949a
Tomasz Grining <tomek.grining@gmail.com>
2024-03-12 10:33:52 +0100

    Redo counts with new db access + more results for Antra

--- components/counts.py
+++ components/counts.py
@@ -1,24 +1,33 @@
 import pandas as pd
 import streamlit as st
 
-from dtower.tourney_results.constants import Graph, Options, champ, league_to_folder
-from dtower.tourney_results.data import get_sus_ids, load_tourney_results
+from dtower.tourney_results.constants import champ
+from dtower.tourney_results.models import TourneyResult, TourneyRow
 
 
-def compute_counts(df, options):
-    df = df[~df.id.isin(get_sus_ids())]
-
+def compute_counts():
     counts_for = [1, 10, 25, 50, 100, 200]
+    limit = 300
+
+    show_all = st.checkbox("Show all results")
+
+    if show_all:
+        counts_for += [300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000]
+        limit = 2100
+
+    champ_results = TourneyResult.objects.filter(league=champ, public=True).order_by("-date")
+
+    rows = TourneyRow.objects.filter(result__in=champ_results, position__lt=limit).order_by("-wave").values("result_id", "wave")
 
     results = []
 
-    for date, ddf in df.groupby("date"):
+    for tourney in champ_results:
+        waves = [row["wave"] for row in rows if row["result_id"] == tourney.id]
         result = {
-            "date": date,
-            "bcs": "/".join([bc.name for bc in ddf.iloc[0].bcs]),
+            "date": tourney.date,
+            "bcs": "/".join([bc.name for bc in tourney.conditions.all()]),
         }
-
-        result |= {f"Top {count_for}": ddf.iloc[count_for - 1].wave if count_for <= len(ddf) else 0 for count_for in counts_for}
+        result |= {f"Top {count_for}": waves[count_for - 1] if count_for <= len(waves) else 0 for count_for in counts_for}
         results.append(result)
 
     to_be_displayed = pd.DataFrame(results).sort_values("date", ascending=False).reset_index(drop=True)
@@ -27,7 +36,4 @@ def compute_counts(df, options):
 
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
-
-    options = Options(links_toggle=False, default_graph=Graph.last_16.value, average_foreground=True)
-    df = load_tourney_results(league_to_folder[champ], result_cutoff=500)
-    compute_counts(df, options)
+    compute_counts()
