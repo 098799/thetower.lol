3dc627d837e6c8a09132ba7a5ac5dc3dc246274e
Tomasz Grining <tomek.grining@gmail.com>
2024-09-23 14:33:19 +0200

    Xsolla things

--- components/player.py
+++ components/player.py
@@ -6,6 +6,8 @@ from urllib.parse import urlencode
 import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
+import streamlit.components.v1 as components
+from cachetools.func import ttl_cache
 from natsort import natsorted
 from plotly.subplots import make_subplots
 
@@ -38,6 +40,140 @@ from dtower.tourney_results.models import TourneyRow
 
 sus_ids = set(SusPerson.objects.filter(sus=True).values_list("player_id", flat=True))
 id_mapping = get_id_lookup()
+
+
+@ttl_cache(maxsize=1000, ttl=6000)
+def get_stones(player_id):
+    import requests
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0",
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/json",
+        "Origin": "https://thetower.xsollasitebuilder.com",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Referer": "https://thetower.xsollasitebuilder.com/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "cross-site",
+        "Priority": "u=0",
+    }
+
+    json_data = {
+        "settings": {
+            "projectId": 264652,
+            "merchantId": 706526,
+        },
+        "loginId": "11f7ad60-c267-4747-9a0d-7613e9711fe5",
+        "webhookUrl": "https://nowebhook.com",
+        "user": {
+            "id": player_id,
+            "country": "CH",
+        },
+        "isUserIdFromWebhook": False,
+    }
+
+    response = requests.post("https://sb-user-id-service.xsolla.com/api/v1/user-id", headers=headers, json=json_data)
+
+    print(response.json())
+
+    token = response.json()["token"]
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0",
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Authorization": f"Bearer {token}",
+        "Origin": "https://thetower.xsollasitebuilder.com",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Referer": "https://thetower.xsollasitebuilder.com/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "cross-site",
+    }
+
+    params = {
+        "locale": "en",
+        "offset": "0",
+    }
+
+    response = requests.get(
+        "https://store.xsolla.com/api/v2/project/264652/items/virtual_items/group/featured",
+        params=params,
+        headers=headers,
+    )
+    print(response.json())
+
+    return response.json()
+
+
+def xsolla_things(player_id, hidden_features, info_tab):
+    stone_info = get_stones(player_id)
+    available = stone_info["items"][1]["limits"]["per_user"]["available"]
+
+    if hidden_features:
+        info_tab.write(f"<img src='./app/static/stones.webp' width='20px'>: {available}/5 available.", unsafe_allow_html=True)
+
+    if available and not hidden_features:
+        html_code = f"""
+        <button id="combinedButton" style="padding: 10px 20px; font-size: 16px; background-color: #FF4B4B; color: white; border: none; border-radius: 5px; cursor: pointer;">Gift stone packs on xsolla!</button>
+
+        <script>
+            document.getElementById('combinedButton').addEventListener('click', async function() {{
+                try {{
+                    // Request to get the token
+                    const tokenResponse = await fetch('https://sb-user-id-service.xsolla.com/api/v1/user-id', {{
+                        method: 'POST',
+                        headers: {{
+                            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0",
+                            "Accept": "application/json, text/plain, */*",
+                            "Accept-Language": "en-US,en;q=0.5",
+                            "Content-Type": "application/json",
+                            "Origin": "https://thetower.xsollasitebuilder.com",
+                            "DNT": "1",
+                            "Connection": "keep-alive",
+                            "Referer": "https://thetower.xsollasitebuilder.com/",
+                            "Sec-Fetch-Dest": "empty",
+                            "Sec-Fetch-Mode": "cors",
+                            "Sec-Fetch-Site": "cross-site",
+                            "Priority": "u=0"
+                        }},
+                        body: JSON.stringify({{
+                            "settings": {{
+                                "projectId": 264652,
+                                "merchantId": 706526,
+                            }},
+                            "loginId": "11f7ad60-c267-4747-9a0d-7613e9711fe5",
+                            "webhookUrl": "https://nowebhook.com",
+                            "user": {{
+                                "id": "{player_id}",
+                                "country": "CH",
+                            }},
+                            "isUserIdFromWebhook": false,
+                        }})
+                    }});
+
+                    if (!tokenResponse.ok) {{
+                        throw new Error('Network response was not ok');
+                    }}
+
+                    const tokenData = await tokenResponse.json();
+                    const token = tokenData.token;
+
+                    // Open the new page with the token
+                    window.open(`https://thetower.xsollasitebuilder.com/?token=${{token}}`, '_blank');
+
+                }} catch (error) {{
+                    console.error('There was a problem:', error);
+                }}
+            }});
+        </script>
+        """
+
+        components.html(html_code, height=100)
 
 
 def compute_player_lookup():
@@ -174,6 +310,9 @@ def compute_player_lookup():
     )
 
     write_for_each_patch(patch_tab, player_df)
+
+    player_id = player_df.iloc[0].id
+    xsolla_things(player_id, hidden_features, info_tab)
 
 
 def filter_lower_leagues(df):
