00791f0d6fa8299ec9957044b71b092696f71117
Tomasz Grining <tomek.grining@gmail.com>
2024-10-02 15:35:26 +0200

    API by user

--- thetower/dtower/thetower/urls.py
+++ thetower/dtower/thetower/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path
 
 from dtower.sus.views import SusView
-from dtower.tourney_results.views import plaintext_results, plaintext_results__champ, results_per_tourney
+from dtower.tourney_results.views import plaintext_results, plaintext_results__champ, results_per_tourney, results_per_user
 
 base_patterns = [path("admin/", admin.site.urls)]
 
@@ -27,6 +27,7 @@ urlpatterns += text_patterns
 
 json_patterns = [
     path("<str:league>/results/<str:tourney_date>/", results_per_tourney),
+    path("player_id/<str:player_id>/", results_per_user),
 ]
 
 urlpatterns += json_patterns
--- thetower/dtower/tourney_results/views.py
+++ thetower/dtower/tourney_results/views.py
@@ -1,5 +1,4 @@
 import datetime
-import os
 from functools import partial
 
 from cachetools import TTLCache, cached
@@ -7,9 +6,10 @@ from django.core.exceptions import BadRequest
 from django.http import HttpResponse, JsonResponse
 from pretty_html_table import build_table
 
+from dtower.sus.models import PlayerId, SusPerson
 from dtower.tourney_results.constants import champ, league_to_folder
-from dtower.tourney_results.data import get_sus_ids, get_tourneys, how_many_results_public_site, load_tourney_results
-from dtower.tourney_results.models import Role, TourneyResult, TourneyRow
+from dtower.tourney_results.data import get_details, get_sus_ids, get_tourneys, how_many_results_public_site, load_tourney_results
+from dtower.tourney_results.models import TourneyResult, TourneyRow
 
 cache = TTLCache(maxsize=10, ttl=600)
 
@@ -46,6 +46,54 @@ def results_per_tourney(request, league, tourney_date):
         return JsonResponse({}, status=404)
 
     df = get_tourneys(qs, offset=0, limit=how_many_results_public_site)
+    df["wave_role"] = df.wave_role.map(lambda x: x.wave_bottom)
+    df["verified"] = df.verified.map(lambda x: int(bool(x)))
+
+    response = [
+        {
+            "id": row.id,
+            "position": row.position,
+            "tourney_name": row.tourney_name,
+            "real_name": row.real_name,
+            "wave": row.wave,
+            "avatar": row.avatar,
+            "relic": row.relic,
+            "date": row.date,
+            "league": row.league,
+            "verified": row.verified,
+            "wave_role": row.wave_role,
+            "patch": str(row.patch),
+        }
+        for _, row in df.iterrows()
+    ]
+
+    return JsonResponse(response, status=200, safe=False)
+
+
+def results_per_user(request, player_id):
+    player_ids = PlayerId.objects.filter(id=player_id)
+    how_many = int(request.GET.get("how_many", 1000))
+
+    if player_ids:
+        player_id = player_ids[0]
+        all_player_ids = player_id.player.ids.all().values_list("id", flat=True)
+        rows = TourneyRow.objects.filter(
+            player_id__in=all_player_ids,
+            result__public=True,
+            position__gt=0,
+        ).order_by(
+            "-result__date"
+        )[:how_many]
+    else:
+        rows = TourneyRow.objects.filter(
+            player_id=player_id,
+            result__public=True,
+            position__gt=0,
+        ).order_by(
+            "-result__date"
+        )[:how_many]
+
+    df = get_details(rows)
     df["wave_role"] = df.wave_role.map(lambda x: x.wave_bottom)
     df["verified"] = df.verified.map(lambda x: int(bool(x)))
 
