7fbcd0d0243d2e160b865bde276abf1adbbd622e
Tomasz Grining <tomek.grining@gmail.com>
2024-10-13 14:14:06 +0200

    Validation bot ocrs and validates

--- discord_bot/discord_bot/validate_id.py
+++ discord_bot/discord_bot/validate_id.py
@@ -1,9 +1,13 @@
+import re
+
+import easyocr
 from asgiref.sync import sync_to_async
 
 from discord_bot.util import get_tower, get_verified_role, verified_role_id
 from dtower.sus.models import KnownPlayer, PlayerId
 
 hex_digits = set("0123456789abcdef")
+reader = easyocr.Reader(["en"])
 
 
 def only_made_of_hex(message):
@@ -12,12 +16,40 @@ def only_made_of_hex(message):
     return contents | hex_digits == hex_digits
 
 
+def hamming_distance(s1, s2):
+    """0 if identical, 1 if completely different"""
+    if len(s1) != len(s2):
+        raise ValueError("Strings must be of equal length")
+    return sum(c1 != c2 for c1, c2 in zip(s1, s2)) / len(s1)
+
+
+async def check_image(content, image_bytes):
+    ocr_results = reader.readtext(image_bytes)
+
+    player_id_candidates = []
+
+    for _, line, _ in ocr_results:
+        subresult = [item for item in re.findall(r"\S{16}", line) if "uppor" not in item]
+
+        if subresult:
+            player_id_candidates.append(subresult[0])
+
+    return any([hamming_distance(candidate, content) < 0.2 for candidate in player_id_candidates])
+
+
 async def validate_player_id(client, message):
-    if message.author.id in [96626874708430848, 177504210177228801, 778131594132062240]:
+    if message.author.id in [96626874708430848, 778131594132062240, 778131594132062240]:
         return
 
     try:
-        if 17 > len(message.content) > 12 and message.attachments and only_made_of_hex(message):
+        if len(message.content) == 16 and message.attachments and only_made_of_hex(message):
+            image_bytes = await message.attachments[0].read()
+
+            if not (await check_image(message.content, image_bytes)):
+                await message.add_reaction("⁉️")
+                await message.add_reaction("🖼️")
+                return
+
             discord_id = message.author.id
 
             player, created = await sync_to_async(KnownPlayer.objects.get_or_create, thread_sensitive=True)(
