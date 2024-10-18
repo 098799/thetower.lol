d2120e287553cdd8e5d1491d2279860e9f3d6f31
ndsimpson <63575139+ndsimpson@users.noreply.github.com>
2024-10-18 14:48:17 -0500

    Sus view no longer used

--- thetower/dtower/thetower/urls.py
+++ thetower/dtower/thetower/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 
-from dtower.sus.views import SusView
 from dtower.tourney_results.views import results_per_tourney, results_per_user
 
 base_patterns = [path("admin/", admin.site.urls)]
@@ -15,10 +14,3 @@ json_patterns = [
 ]
 
 urlpatterns += json_patterns
-
-
-sus_patterns = [
-    path("sus/", SusView.as_view()),
-]
-
-urlpatterns += sus_patterns
