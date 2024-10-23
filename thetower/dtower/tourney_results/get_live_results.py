434fc142c484cf10b2a00c2aff38df841b608604
ndsimpson <63575139+ndsimpson@users.noreply.github.com>
2024-10-22 20:44:26 -0500

    Fix deperecation

--- thetower/dtower/tourney_results/get_live_results.py
+++ thetower/dtower/tourney_results/get_live_results.py
@@ -22,7 +22,7 @@ logging.basicConfig(level=logging.INFO)
 
 def get_current_time__game_server():
     """Game server runs on utc time and we want live results right away, minus other built-in delays."""
-    return datetime.datetime.utcnow()
+    return datetime.timezone.utc
 
 
 def get_date_offset() -> int:
