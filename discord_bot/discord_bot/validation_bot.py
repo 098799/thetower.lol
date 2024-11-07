f295c13eb7f0ae8cb920138091f0a82ebfe8c7f9
ndsimpson <63575139+ndsimpson@users.noreply.github.com>
2024-11-07 09:24:11 -0600

    One more for consistency

--- discord_bot/discord_bot/validation_bot.py
+++ discord_bot/discord_bot/validation_bot.py
@@ -12,7 +12,7 @@ django.setup()
 from asgiref.sync import sync_to_async
 
 from discord_bot.print_role_counts import print_roles
-from discord_bot.util import is_player_id_please_channel, is_role_count_channel, is_t50_channel, is_testing_channel
+from discord_bot.util import is_player_id_please_channel, is_role_count_channel, is_top50_channel, is_testing_channel
 from discord_bot.validate_id import validate_player_id
 from dtower.sus.models import KnownPlayer, PlayerId
 from dtower.tourney_results.models import Injection
@@ -66,7 +66,7 @@ async def on_message(message):
                 logging.exception(exc)
         elif (is_testing_channel(message.channel) or is_role_count_channel(message.channel)) and message.content.startswith("!role_counts"):
             await print_roles(client, message)
-        elif is_t50_channel(message.channel) and message.content.startswith("!inject"):
+        elif is_top50_channel(message.channel) and message.content.startswith("!inject"):
             if const.top1_id in {role.id for role in message.author.roles}:
                 injection = message.content.split(" ", 1)[1]
                 Injection.objects.create(text=injection, user=message.author.id)
