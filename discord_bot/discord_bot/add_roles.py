e06ecbbcf341b1e470b3dd3e0ebabd78c70ad1db
Tomasz Grining <tomek.grining@gmail.com>
2024-11-08 23:00:03 +0100

    Maybe now

--- discord_bot/discord_bot/add_roles.py
+++ discord_bot/discord_bot/add_roles.py
@@ -10,7 +10,7 @@ from asgiref.sync import sync_to_async
 from discord_bot import const
 from discord_bot.util import get_all_members, get_tower, role_id_to_position
 from dtower.sus.models import KnownPlayer, PlayerId, SusPerson
-from dtower.tourney_results.constants import champ, copper, gold, leagues, legend, plat, silver
+from dtower.tourney_results.constants import champ, copper, gold, how_many_results_hidden_site, leagues, legend, plat, silver
 from dtower.tourney_results.data import get_results_for_patch, get_tourneys
 from dtower.tourney_results.models import PatchNew as Patch
 
@@ -58,13 +58,15 @@ async def handle_adding(
 
     dfs = {}
 
-    dfs[leagues[0]] = get_tourneys(get_results_for_patch(patch=patch, league=leagues[0]), limit=1500)  # legend roles go up to 1.5k
+    dfs[leagues[0]] = get_tourneys(
+        get_results_for_patch(patch=patch, league=leagues[0]), limit=how_many_results_hidden_site
+    )  # Need to know everyone in legends to fallback to champ 500 in case they don't qualify for
 
     if verbose:
         await debug_channel.send(f"Loaded legends tourney data of {len(dfs[leagues[0]])} rows")
 
     for league in leagues[1:]:
-        dfs[league] = get_tourneys(get_results_for_patch(patch=patch, league=league), limit=20000)
+        dfs[league] = get_tourneys(get_results_for_patch(patch=patch, league=league), limit=how_many_results_hidden_site)
 
         if verbose:
             await debug_channel.send(f"Loaded {league} tourney data of {len(dfs[league])} rows")
@@ -139,6 +141,11 @@ async def handle_adding(
 
                     if not role_assigned:  # doesn't qualify for legend role but has some results in legends
                         role = wave_roles_by_league[champ][500]
+
+                        if role in discord_player.roles:
+                            unchanged[legend].append((discord_player, role))
+                            break
+
                         other_roles = [other_role for other_role in wave_roles if other_role != role]
                         await add_wave_roles(changed, discord_player, champ, 500, role)
                         role_assigned = True
